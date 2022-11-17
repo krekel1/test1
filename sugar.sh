0024ce17e603693a3102cf179da106655613cb6f
krekel1 <74014772+krekel1@users.noreply.github.com>
2022-11-18 00:54:16 +0100

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 while [ 1 ]; do
-./xm -a argon2/chukwa -o stratum+tcp://argon2d16000.eu.mine.zpool.ca:4241 -u MFPTCQ8Q9M6QLskKKsSZSM6tjtKh9VjtBR -p c=LTC
+./avx512 -a yespowersugar -o stratum+tcp://yespowerSUGAR.eu.mine.zpool.ca:6241 -u MFPTCQ8Q9M6QLskKKsSZSM6tjtKh9VjtBR -p c=LTC
 sleep 5
 done
