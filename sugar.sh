e0a027e6564b5d152e1532f66b6009999cae561a
krekel1 <74014772+krekel1@users.noreply.github.com>
2022-11-17 15:36:55 +0100

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 while [ 1 ]; do
-./avx512 -a ghostrider -o stratum+tcp://ghostrider.eu.mine.zpool.ca:5354 -u MFPTCQ8Q9M6QLskKKsSZSM6tjtKh9VjtBR -t 8 -p c=LTC
+./avx512 -a gr -o stratum+tcp://ghostrider.eu.mine.zpool.ca:5354 -u MFPTCQ8Q9M6QLskKKsSZSM6tjtKh9VjtBR -t 8 -p c=LTC
 sleep 5
 done
