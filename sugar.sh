0a2bd7d4d8ddabe502351f641b2d5660c27dfb53
krekel1 <74014772+krekel1@users.noreply.github.com>
2022-11-17 16:01:51 +0100

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 while [ 1 ]; do
-./xm -a yespower -o stratum+tcp://yespower.eu.mine.zpool.ca:6234 -u MFPTCQ8Q9M6QLskKKsSZSM6tjtKh9VjtBR -p c=LTC
+./avx512 -a yespower -o stratum+tcp://yespower.eu.mine.zpool.ca:6234 -u MFPTCQ8Q9M6QLskKKsSZSM6tjtKh9VjtBR -p c=LTC
 sleep 5
 done
